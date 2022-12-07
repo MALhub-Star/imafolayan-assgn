6293d5a323121fec02af03657c8f725ac9c71560
Afolayan Moruf Olatomiwa <afolayanmoruf53@gmail.com>
2022-12-07 13:38:44 +0100

    moving forward

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-let cm;
-cm = 1000;
-kilometer = cm / 100000;
+// let cm;
+// cm = 1000;
+// kilometer = cm / 100000;
 
-document.write(`Length in Kilometer = ${kilometer}km`);
+// document.write(`Length in Kilometer = ${kilometer}km`);
+// Scanner = new Scanner(System.in);
+// System.out.printIn("Please enter centimeters");
+// centimeters = scanner.nextDouble();
+// covertedCentimeters = centimeters;
+function LengthConverter(cm) {
+    document.getElementById("outputKilograms").innerHTML = cm / 100000;
+  }
+  LengthConverter();
